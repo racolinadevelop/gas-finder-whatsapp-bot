35d8da2540f06ec09e1dcc5189bb79018fded645
racolinadevelop <122189936+racolinadevelop@users.noreply.github.com>
2026-09-20 18:46:42 -0400

    Cover main menu export and isolate language persistence between tests

--- app/presentation/__init__.py
+++ app/presentation/__init__.py
@@ -25,6 +25,7 @@ __all__ = [
     "build_fuel_prompt",
     "build_language_prompt",
     "build_location_prompt",
+    "build_main_menu_prompt",
     "build_navigation_prompt",
     "build_results_navigation_prompt",
     "build_sort_prompt",
